f06cfd736bba9f497a7ad23f48ed891d9e142113
장문기 <jangmoonki@jangmungiui-noteubug.local>
2024-10-28 23:00:33 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember () throws Exception {
+
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        Long savedId =  memberRepository.save(member);
+
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertEquals(member.getUsername(), findMember.getUsername());
+        Assertions.assertEquals(member.getId(), findMember.getId());
+        Assertions.assertEquals(findMember, member);
+    }
+}
