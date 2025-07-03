7b80188e50cd87385ddb0059c71a01d553a1b53c
jangmoonki <ansrlwkd225@naver.com>
2025-07-03 15:43:35 +0900

    JPA API 성능 최적화 방법

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,22 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
     public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+    }
+
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module module = new Hibernate5Module();
+        module.disable(Hibernate5Module.Feature.USE_TRANSIENT_ANNOTATION);
+        return module;
     }
 
 }
